edf619cc73655251e2304236ff7d04fbe41da276
刘智宇 <wyliuzhiyu@zbmac-fd89c27bf.360buyad.local>
2019-10-25 14:59:24 +0800

    Initial commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+"""
+第一个Python程序 - hello, world!
+向伟大的Dennis M. Ritchie先生致敬
+
+Version: 0.1
+Author: 骆昊
+"""
+
+print('hello, world!')
+# print("你好,世界！")
+print('你好', '世界')
+print('hello', 'world', sep=', ', end='!')
+print('goodbye, world', end='!\n')
